51f0efc4c589f40d01c092ded00f06c543ec559f
SimoneBorrometi <simoborrometi@gmail.com>
2024-08-25 20:33:21 +0200

    Flink retrieves data from Kafka using Jackson JSON deserializer. Added a logstash filter for the duplicate message in event/original

--- flink/mailscanner/src/main/java/org/dmi/mailscanner/DataStreamJob.java
+++ flink/mailscanner/src/main/java/org/dmi/mailscanner/DataStreamJob.java
@@ -16,17 +16,17 @@
  * limitations under the License.
  */
 
-package org.dmi.mailscanner;
+ package org.dmi.mailscanner;
 
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
-import org.apache.flink.api.common.functions.FlatMapFunction;
-import org.apache.flink.api.common.serialization.SimpleStringSchema;
-import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
+import org.apache.flink.formats.json.JsonDeserializationSchema;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.DeserializationFeature;
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.flink.streaming.api.datastream.DataStream;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
-import org.apache.flink.util.Collector;
+
 
 /**
  * Skeleton for a Flink DataStream Job.
@@ -50,39 +50,29 @@ public class DataStreamJob {
 		final StreamExecutionEnvironment env = StreamExecutionEnvironment.getExecutionEnvironment();
 		
 
-		KafkaSource<String> source;
-            source = KafkaSource.<String>builder()
+
+        JsonDeserializationSchema<Email> jsonFormat=new JsonDeserializationSchema<>(
+            Email.class,
+            () -> new ObjectMapper()
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+            );
+
+		KafkaSource<Email> source;
+            source = KafkaSource.<Email>builder()
                     .setBootstrapServers(bootstrapServer)
                     .setTopics("mails")
                     .setGroupId("org.dmi.mailscanner")
                     .setStartingOffsets(OffsetsInitializer.earliest())
-                    .setValueOnlyDeserializer(new SimpleStringSchema())
+                    .setValueOnlyDeserializer(jsonFormat)
                     .build();
 
 		env.fromSource(source, WatermarkStrategy.noWatermarks(), "Kafka Source");
 
-		DataStream<String> text = env.fromSource(source, WatermarkStrategy.noWatermarks(), "kafka-input");
+		DataStream<Email> emails = env.fromSource(source, WatermarkStrategy.noWatermarks(), "kafka-input");
 
-		DataStream<Tuple2<String, Integer>> counts =
-                // The text lines read from the source are split into words
-                // using a user-defined function. The tokenizer, implemented below,
-                // will output each word as a (2-tuple) containing (word, 1)
-                text.flatMap(new Tokenizer())
-                        .name("tokenizer")
-                        // keyBy groups tuples based on the "0" field, the word.
-                        // Using a keyBy allows performing aggregations and other
-                        // stateful transformations over data on a per-key basis.
-                        // This is similar to a GROUP BY clause in a SQL query.
-                        .keyBy(value -> value.f0)
-                        // For each key, we perform a simple sum of the "1" field, the count.
-                        // If the input data stream is bounded, sum will output a final count for
-                        // each word. If it is unbounded, it will continuously output updates
-                        // each time it sees a new instance of each word in the stream.
-                        .sum(1)
-                        .name("counter");
 
-		counts.print().name("print-sink");
-
+		emails.print().name("print-sink");
+    
 
 		env.execute("Flink Java API Skeleton");
 	}
@@ -90,22 +80,4 @@ public class DataStreamJob {
     public static String getBootstrapServer() {
         return bootstrapServer;
     }
-
-	public static final class Tokenizer
-            implements FlatMapFunction<String, Tuple2<String, Integer>> {
-
-        @Override
-        public void flatMap(String value, Collector<Tuple2<String, Integer>> out) {
-            // normalize and split the line
-            String[] tokens = value.toLowerCase().split("\\W+");
-
-            // emit the pairs
-            for (String token : tokens) {
-                if (token.length() > 0) {
-                    out.collect(new Tuple2<>(token, 1));
-                }
-            }
-        }
-    }
-
 }
